198a0a0fe47fd50ac983f4b7b2420f41b8372e08
Lucas Buchholz <lucas.buchholz1@gmail.de>
2023-10-16 22:05:57 +0200

    ci: bot.command implementation

--- src/controller/BotController.py
+++ src/controller/BotController.py
@@ -3,12 +3,12 @@ import discord
 import os
 from dotenv import load_dotenv
 import src.handler.AiHandler as AiHandler
+from discord.ext import commands
 
 # Generate Discord-Bot client
 intents = discord.Intents.default()
 intents.message_content = True
-
-bot = discord.Client(intents=intents)
+bot = commands.Bot(intents=intents, command_prefix='f!')
 
 logging.basicConfig(level=logging.INFO)
 
@@ -22,24 +22,33 @@ def start_bot():
     bot.run(os.getenv('dc_key'))
 
 
+@bot.command()
+async def test(ctx, *, arg):
+    await ctx.send(arg)
+
+
 @bot.event
 async def on_ready():
-    logging.info("Bot is online")
+    print(f'Bot ist nun online: {bot.user.name}')
+
+
+@bot.command()
+async def hallo(ctx):
+    await ctx.send('Hallo!')
 
 
 @bot.event
 async def on_message(msg):
     if msg.author == bot.user:
         return
-    logging.info("[" + str(msg.author) + "]," + msg.content)
+    logging.info("[" + str(msg.author) + "] " + msg.content)
 
     #
     if msg.content.startswith('C:'):
         question = msg.content
         answer = AiHandler.generate_completion(question)
 
-
         for part in answer:
             logging.info("text-davinci-003: " + part)
             await msg.channel.send(part)
-
+    await bot.process_commands(msg)
