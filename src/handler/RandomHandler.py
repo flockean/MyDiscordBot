259a53f7cc70d8623508758282fe61fceee299b5
Lucas Buchholz <lucas.buchholz1@gmail.de>
2023-10-17 21:00:46 +0200

    ci: random.py

---
+++ src/handler/RandomHandler.py
@@ -0,0 +1,31 @@
+import json
+import random
+import requests
+
+
+def random_number(x, y):
+    return random.randint(x, y)
+
+
+def random_word(arr=None):
+    if arr is None:
+        arr = []
+    return random.choice(arr)
+
+# Not yet really correct
+def random_joke(category=None):
+
+    if category is None:
+        api_url = "https://witzapi.de/api/joke"
+    if category != None:
+        api_url = "https://witzapi.de/api/joke?" + category
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        data = response.json()
+        text_value = data[0]["text"]
+        return text_value
+    return "Sorry, Joke fabric down"
+
+
+
+print(random_joke())
