dc8e8ae1474c4c3c520d6a8f97911efb38783c3a
flockean <lucas.buchholz1@gmail.com>
2024-10-13 21:59:21 +0200

    ci: update services

--- src/cogs/game_mgmt.py
+++ src/cogs/game_mgmt.py
@@ -3,12 +3,13 @@ from discord.ext import commands
 from src.controller.util_service import Category, fancy_enumeration, fancy_enumeration_categorys
 from src.database import database_utils
 from src.database.database_utils import *
-from src.models.schemas import GameProgress, GameType, ProgressStatus
+from src.service.game_mgmt_service import get_all_games, get_all_types, create_game, create_type
+from src.models.schemas import Gameprogress, Gamegenre, ProgressStatus
 import re
 import logging
 
-class GameMmgt(commands.Cog):
 
+class GameMmgt(commands.Cog):
     input_single = r'(\w+)'
     input_full = r'"(\w+:\w+")'
     input_full_game = r'(\w+:)'
@@ -16,46 +17,50 @@ class GameMmgt(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+
+
     # TODO: does not work at please fix
-    @commands.command(usage=Category.management)
+    @commands.command(usage=Category.management, name="getGames")
     async def get_games(self, ctx):
         logging.info("getgames has been triggerd")
-        games = database_utils.get_all(GameProgress)
+        games = get_all_games(get_db())
+        if games == None:
+            await ctx.send("Sorry Nothing there")
         logging.info(games)
         await ctx.send(games)
 
-    @commands.command(usage=Category.management)
+    @commands.command(usage=Category.management, name="getCats")
     async def get_category(self, ctx):
-        await ctx.send(
-            fancy_enumeration(database_utils.get_all(GameType)))
+        categorys = get_all_types(get_db())
+        if categorys == None:
+            await ctx.send("Sorry Nothing there")
+        logging.info(categorys)
+        await ctx.send(categorys)
 
-    @commands.command(usage=Category.management)
+    @commands.command(usage=Category.management, name="setCat")
     async def set_category(self, ctx, *, args):
         logging.info(args)
         matches = re.findall(self.input_single, args)
         if matches:
             logging.info(matches)
             for match in matches:
-                database_utils.add(
-                    GameType(id_name=match)
-                )
-        await ctx.send("Category has been added")
+                create_type(Gamegenre(name=match), get_db())
+        await ctx.send("Category created")
 
     # TODO: does not work yet
-    @commands.command(usage=Category.management)
+    @commands.command(usage=Category.management, name="setGame")
     async def set_game(self, ctx, *, args):
         matches = re.findall(self.input_single, args)
         if matches:
             logging.info(matches)
             for match in matches:
-                database_utils.add(
-                GameProgress(id_name=match,
-                             game_type="Hamburger",
-                             in_progress=ProgressStatus.notStarted)
-                )
+                create_game(Gameprogress(name=match,
+                                         type_id="Hamburger",
+                                         in_progress=ProgressStatus.notStarted
+                                         ), get_db())
+
         await ctx.send("Game has been added")
 
 
 async def setup(client):
     await client.add_cog(GameMmgt(client))
-
--- src/models/schemas.py
+++ src/models/schemas.py
@@ -1,4 +1,6 @@
 import datetime
+import enum
+import uuid
 from datetime import datetime
 from enum import Enum
 
@@ -7,49 +9,59 @@ from sqlalchemy.orm import DeclarativeBase, relationship
 from sqlalchemy.orm import Mapped, mapped_column
 
 
+def get_uuid() -> str:
+    return str(uuid.uuid4())
+
+
 class Base(DeclarativeBase):
     pass
 
+
 class DMMessage(Base):
     __tablename__ = "dmmessage"
-    id: Mapped[datetime] = mapped_column(primary_key=True)
+    id: Mapped[str] = mapped_column(primary_key=True, default=get_uuid())
+    time: Mapped[datetime]
     author: Mapped[str]
     content: Mapped[str]
-
-    def __init__(self, author, content):
-        self.id = datetime.now()
-        self.author = author
-        self.content = content
 
     def __repr__(self):
         return f'{self.id} - {self.author} - {self.content} \n'
 
-class GameProgress(Base):
-    __tablename__ = "game_progress"
-    id_name: Mapped[str] = mapped_column(primary_key=True)
-    game_type: Mapped[str] = mapped_column(ForeignKey("game_type.id_name"))
+
+class Gamegenre(Base):
+    __tablename__ = "gamegenre"
+    id: Mapped[str] = mapped_column(primary_key=True, default=get_uuid())
+    name: Mapped[str]
+
+    games: Mapped[list["Gameprogress"]] = relationship(back_populates="game_type")
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self) -> str:
+        return f"{self.name}"
+
+
+class Gameprogress(Base):
+    __tablename__ = "gameprogress"
+    id: Mapped[str] = mapped_column(primary_key=True, default=get_uuid())
+    name: Mapped[str] = mapped_column(unique=True)
+    type_id: Mapped[str] = mapped_column(ForeignKey("gamegenre.id"))
     in_progress: Mapped[int]
 
-    game_category: Mapped["GameType"] = relationship(back_populates="game_progress", primaryjoin="GameProgress.game_type==GameType.id_name")
+    game_type: Mapped["Gamegenre"] = relationship(back_populates="games",
+                                                  primaryjoin="Gameprogress.type_id==Gamegenre.id")
 
-    def __init__(self, id_name, game_type, in_progress):
-        self.id_name = id_name
-        self.game_type = game_type
+    def __init__(self, name, type_id, in_progress):
+        self.name = name
+        self.type_id = type_id
         self.in_progress = in_progress
 
     def __repr__(self):
-        return f'> {self.in_progress} {self.id_name}'
+        return f'> {self.name} {self.in_progress}'
 
 
-class GameType(Base):
-    __tablename__ = "game_type"
-    id_name: Mapped[str] = mapped_column(primary_key=True)
-
-    def __repr__(self) -> str:
-        return f"{self.id_name}"
-
-class ProgressStatus(Enum):
+class ProgressStatus(enum.Enum):
     notStarted = 1
     inProgress = 2
     finished = 3
-
